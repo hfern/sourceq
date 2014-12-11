f49fd8aa32c58d79009b60fb5cfca38cbe3d2040
Hunter <h.g.fern@gmail.com>
2014-12-10 21:52:58 -0800

    Add players section to ss

--- servertextview.go
+++ servertextview.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"sort"
+	"strconv"
 	"strings"
+	"time"
 )
 
 type Ident struct {
@@ -33,6 +35,107 @@ func textFormatServer(server ServerAttrPair, ident Ident, options *ServerQueryOp
 	ident.level++
 
 	textFormatServerInfo(server.Attrs.Info, ident, options)
+	textFormatPlayers(server.Attrs.Players, ident, options)
+}
+
+func textFormatPlayers(players MaybePlayers, ident Ident, options *ServerQueryOptions) {
+	if options.NoPlayers {
+		return
+	}
+
+	ident.Println("Players:")
+	ident.level++
+
+	if players.Error != nil {
+		ident.Println("Error fetching player list: ", players.Error.Error())
+		return
+	}
+
+	type PlayerRow struct {
+		RowID    string
+		Name     string
+		Index    string
+		Score    string
+		Duration string
+	}
+
+	plrRows := make([]PlayerRow, 0, len(players.Players)+1)
+	plrRows = append(plrRows, PlayerRow{
+		RowID:    "  ",
+		Name:     "Name",
+		Index:    "Id",
+		Score:    "Scr",
+		Duration: "Time",
+	})
+
+	for i, player := range players.Players {
+		col := PlayerRow{
+			RowID: strconv.Itoa(i + 1),
+			Name:  player.Name(),
+			Index: strconv.Itoa(player.Index()),
+			Score: strconv.Itoa(player.Score()),
+			// round to 1s
+			Duration: (player.Duration() - (player.Duration() % time.Second)).String(),
+		}
+		plrRows = append(plrRows, col)
+	}
+
+	largest := func(things []PlayerRow, stringer func(PlayerRow) string) int {
+		max := 0
+		for _, thing := range things {
+			sz := len(stringer(thing))
+			if sz > max {
+				max = sz
+			}
+		}
+		return max
+	}
+
+	maxColumnSizes := struct {
+		RowID    int
+		Name     int
+		Index    int
+		Score    int
+		Duration int
+	}{
+		RowID:    largest(plrRows, func(r PlayerRow) string { return r.RowID }),
+		Name:     largest(plrRows, func(r PlayerRow) string { return r.Name }),
+		Index:    largest(plrRows, func(r PlayerRow) string { return r.Index }),
+		Score:    largest(plrRows, func(r PlayerRow) string { return r.Score }),
+		Duration: largest(plrRows, func(r PlayerRow) string { return r.Duration }),
+	}
+
+	for i, row := range plrRows {
+		ident.Printf(
+			" %v | %v | %v | %v | %v \n",
+			paddedR(row.RowID, maxColumnSizes.RowID),
+			padded(row.Name, maxColumnSizes.Name),
+			padded(row.Index, maxColumnSizes.Index),
+			paddedR(row.Score, maxColumnSizes.Score),
+			padded(row.Duration, maxColumnSizes.Duration),
+		)
+
+		if i == 0 {
+			lengths := []int{
+				maxColumnSizes.RowID,
+				maxColumnSizes.Name,
+				maxColumnSizes.Index,
+				maxColumnSizes.Score,
+				maxColumnSizes.Duration,
+			}
+			divider := ""
+
+			for j, length := range lengths {
+				if j != 0 {
+					divider = divider + "+"
+				}
+				divider = divider + strings.Repeat("-", length+2)
+			}
+
+			ident.Printf("%s\n", divider)
+		}
+	}
+	ident.Println("")
 }
 
 func textFormatServerInfo(info MaybeInfo, ident Ident, options *ServerQueryOptions) {
@@ -72,10 +175,6 @@ func textFormatServerInfo(info MaybeInfo, ident Ident, options *ServerQueryOptio
 		}
 	}
 
-	pad := func(n int) string {
-		return strings.Repeat(" ", n)
-	}
-
 	filters := map[string]func(interface{}) interface{}{
 		"SteamID": func(interface{}) interface{} {
 			return info.Info.GetSteamID()
@@ -85,6 +184,9 @@ func textFormatServerInfo(info MaybeInfo, ident Ident, options *ServerQueryOptio
 	keys := getKeys(mapped)
 	sort.Strings(keys)
 
+	ident.prompt = "| "
+	ident.promptActive = true
+
 	for _, key := range keys {
 		val := mapped[key]
 
@@ -92,8 +194,11 @@ func textFormatServerInfo(info MaybeInfo, ident Ident, options *ServerQueryOptio
 			val = filters[key](val)
 		}
 
-		ident.Printf("| %s:%s %v\n", key, pad(maxKeySize-len(key)), val)
+		ident.Printf("%s:%s %v\n", key, pad(maxKeySize-len(key)), val)
 	}
+
+	ident.promptActive = false
+	ident.Println("")
 }
 
 func (ident *Ident) Println(args ...interface{}) {
@@ -101,13 +206,17 @@ func (ident *Ident) Println(args ...interface{}) {
 	fmt.Println(args...)
 }
 
-func (ident *Ident) Printf(format string, args ...interface{}) {
+func (ident *Ident) Printf(format string, args ...interface{}) (int, error) {
 	fmt.Print(ident.GetPrefix())
-	fmt.Printf(format, args...)
+	return fmt.Printf(format, args...)
 }
 
 func (ident *Ident) GetPrefix() string {
-	return strings.Repeat(ident.padding, ident.level)
+	cursor := ""
+	if ident.promptActive {
+		cursor = ident.prompt
+	}
+	return strings.Repeat(ident.padding, ident.level) + cursor
 }
 
 func getKeys(amap map[string]interface{}) []string {
@@ -116,4 +225,16 @@ func getKeys(amap map[string]interface{}) []string {
 		keys = append(keys, key)
 	}
 	return keys
+}
+
+func pad(n int) string {
+	return strings.Repeat(" ", n)
+}
+
+func padded(text string, padLength int) string {
+	return text + pad(padLength-len(text))
+}
+
+func paddedR(text string, padLength int) string {
+	return pad(padLength-len(text)) + text
 }
